9fbd8e49c81f238775a6b761890c27cccce78f2e
rathipremkumar15 <rathipremkumar1@gmail.com>
2026-09-15 23:55:06 +0530

    Add Spring Boot application entry point

---
+++ backend/src/main/java/com/socialmoneymanager/SocialMoneyManagerApplication.java
@@ -0,0 +1,11 @@
+package com.socialmoneymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMoneyManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMoneyManagerApplication.class, args);
+    }
+}
