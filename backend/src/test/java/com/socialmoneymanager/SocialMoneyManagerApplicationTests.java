e335e5fa14a386ea20664f0ed68564597407839f
rathipremkumar15 <rathipremkumar1@gmail.com>
2026-09-15 23:59:02 +0530

    Add Spring Boot foundation context test

---
+++ backend/src/test/java/com/socialmoneymanager/SocialMoneyManagerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.socialmoneymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMoneyManagerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
